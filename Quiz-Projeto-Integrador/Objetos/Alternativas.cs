3f8cb7583e3a9459de312d8d71e9a0974d7127d6
LUIS FELIPE KURTZ LEAO <05659679080@senacrs.edu.br>
2026-08-20 19:31:27 -0300

    Inicio do quiz

--- Quiz-Projeto-Integrador/Objetos/Alternativas.cs
+++ Quiz-Projeto-Integrador/Objetos/Alternativas.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Quiz_Projeto_Integrador.Objetos
+﻿namespace Quiz_Projeto_Integrador.Objetos
 {
     public class Alternativas
     {
-        public int Id { get; set; }
-        public string Pontos {  get; set; }
-        public string PerguntaId { get; set; }
+        public int Pontos {  get; set; }
         public string Correta { get; set; }
         public string EscolhaA { get; set; }
         public string EscolhaB { get; set; }
